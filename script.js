6466936729ddfdb7d55eabda0874525d9be43095
James Hawker <526rserbinenko@frhsd.com>
2025-09-13 16:31:19 +0000

    screwed something up, modifying the quadtree algorithm for better function

--- script.js
+++ script.js
@@ -122,56 +122,113 @@ class Game {
         this.TREE = this.make(this.DATA);
         this.FOUND = false;
       }
-      make(dataset) {
-        if (!dataset || dataset.length <= 0 || !Array.isArray(dataset)) return;
+      // make(dataset) {
+      //   if (!dataset || dataset.length <= 0 || !Array.isArray(dataset)) return;
+      //   const self = this;
+      //   let [mX, mY] = dataset[0];
+      //   let [mxX, mxY] = dataset[0];
+      //   dataset.forEach(([x, y]) => {
+      //     if (x < mX) mX = x;
+      //     else if (x > mxX) mxX = x;
+      //     if (y < mY) mY = y;
+      //     else if (y > mxY) mxY = y;
+      //   });
+      //   const dX = (mxX - mX) / 2 + mX;
+      //   const dY = (mxY - mY) / 2 + mY;
+      //   class Node {
+      //     constructor(xInit, xFinal, yInit, yFinal) {
+      //       this.CLOUD = [];
+      //       this.SUB = null;
+      //       this.xRange = [xInit, xFinal];
+      //       this.yRange = [yInit, yFinal];
+      //     }
+      //     divide() {
+      //       if (this.CLOUD.length <= self.LeafThreshold) return;
+      //       else this.SUB = self.make(this.CLOUD);
+      //     }
+      //   }
+      //   let Nodes = [
+      //     new Node(mX, dX, mY, dY),
+      //     new Node(dX + 0.001, mxX, mY, dY),
+      //     new Node(mX, dX, dY + 0.001, mxY),
+      //     new Node(dX + 0.001, mxX, dY + 0.001, mxY),
+      //   ];
+      //   dataset.forEach((e) => {
+      //     Nodeloop: for (let q = 0; q < Nodes.length; q++) {
+      //       const { xRange, yRange } = Nodes[q];
+      //       const [x, y] = e;
+      //       if (
+      //         x >= xRange[0] &&
+      //         x <= xRange[1] &&
+      //         y >= yRange[0] &&
+      //         y <= yRange[1]
+      //       ) {
+      //         Nodes[q].CLOUD.push(e);
+      //         break Nodeloop;
+      //       }
+      //     }
+      //   });
+      //   Nodes = Nodes.filter((e) => {
+      //     if (e.CLOUD.length <= 0) return false;
+      //     else e.divide();
+      //     return true;
+      //   });
+      //   return Nodes;
+      // }
+      make(set, [xR1, xR2, yR1, yR2] = new Array(4).fill(undefined)) {
         const self = this;
-        let [mX, mY] = dataset[0];
-        let [mxX, mxY] = dataset[0];
-        dataset.forEach(([x, y]) => {
-          if (x < mX) mX = x;
-          else if (x > mxX) mxX = x;
-          if (y < mY) mY = y;
-          else if (y > mxY) mxY = y;
-        });
+        let [mX, mY, mxX, mxY] = [...set[0], ...set[0]];
+        if ([xR1, xR2, yR1, yR2].every((e) => e !== undefined)) {
+          [mX, mxX, mY, mxY] = [xR1, xR2, yR1, yR2];
+        } else {
+          set.forEach(([x, y]) => {
+            (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
+            (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
+          });
+        }
         const dX = (mxX - mX) / 2 + mX;
         const dY = (mxY - mY) / 2 + mY;
         class Node {
-          constructor(xInit, xFinal, yInit, yFinal) {
-            this.CLOUD = [];
+          constructor() {
+            const inSelf = this;
             this.SUB = null;
-            this.xRange = [xInit, xFinal];
-            this.yRange = [yInit, yFinal];
+            this.xRange = [Infinity, -Infinity];
+            this.yRange = [Infinity, -Infinity];
+            this.CLOUD = new Proxy([], {
+              get(target, property) {
+                if (property === "push") {
+                  return function (...args) {
+                    if (args.every((e) => e.length === 3)) {
+                      let [mX, mxX] = inSelf.xRange;
+                      let [mY, mxY] = inSelf.yRange;
+                      args.forEach(([x, y]) => {
+                        (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
+                        (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
+                      });
+                      inSelf.xRange = [mX, mxX];
+                      inSelf.yRange = [mY, mxY];
+                    }
+                    return Array.prototype.push.apply(target, args);
+                  };
+                } else return Reflect.get(target, property);
+              },
+            });
           }
           divide() {
-            if (this.CLOUD.length <= self.LeafThreshold) return;
-            else this.SUB = self.make(this.CLOUD);
+            if (this.CLOUD.length < self.LeafThreshold) return false;
+            this.SUB = self.make(this.CLOUD, [...this.xRange, ...this.yRange]);
           }
         }
-        let Nodes = [
-          new Node(mX, dX, mY, dY),
-          new Node(dX + 0.001, mxX, mY, dY),
-          new Node(mX, dX, dY + 0.001, mxY),
-          new Node(dX + 0.001, mxX, dY + 0.001, mxY),
-        ];
-        dataset.forEach((e) => {
-          Nodeloop: for (let q = 0; q < Nodes.length; q++) {
-            const { xRange, yRange } = Nodes[q];
-            const [x, y] = e;
-            if (
-              x >= xRange[0] &&
-              x <= xRange[1] &&
-              y >= yRange[0] &&
-              y <= yRange[1]
-            ) {
-              Nodes[q].CLOUD.push(e);
-              break Nodeloop;
-            }
-          }
+        let Nodes = new Array(8).fill(null).map(() => new Node());
+        set.forEach(([x, y]) => {
+          const index = (x > dX ? 1 : 0) | (y > dY ? 2 : 0);
+          Nodes[index].CLOUD.push([x, y]);
         });
-        Nodes = Nodes.filter((e) => {
-          if (e.CLOUD.length <= 0) return false;
-          else e.divide();
-          return true;
+        Nodes = Nodes.filter((Node) => {
+          if (Node.CLOUD.length > 0) {
+            Node.divide();
+            return true;
+          } else return false;
         });
         return Nodes;
       }
@@ -217,7 +274,13 @@ class Game {
   async init(LUT) {
     await this.LUT_init(LUT);
     this.COLORTREE = new this.Octree(this.LUT);
-    log(this.COLORTREE.search([111, 111, 111]));
+    // log(this.COLORTREE.search([111, 111, 111]));
+    const myQuad = new this.Quadtree(
+      new Array.fill(null).map((_, i) => {
+        i, i + 1;
+      })
+    );
+    log(myQuad.search([0, 0]));
   }
   async LUT_init(LUT) {
     const text = await (await fetch(LUT)).text();
