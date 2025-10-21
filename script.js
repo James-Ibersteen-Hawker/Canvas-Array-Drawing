c2230a41d9039aa1ae1cfdca65fb71ac527f5e09
James Hawker <526rserbinenko@frhsd.com>
2025-10-21 18:59:15 +0000

    optimising for speed. I feel the NEED FOR SPEED

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ class Game {
     this.config = null;
     (this.LUT = []), (this.LUT_LUT = new Map());
     (this.CANVAS = CANVAS), (this.CTX = this.CANVAS.getContext("2d"));
+
     this.Octree = class {
       constructor(dataset) {
+        const self = this;
         this.DATA = dataset;
         this.LeafThreshold = 4;
-        this.TREE = this.make(this.DATA);
         this.PREVIOUS = undefined;
         this.xRange = [Infinity, -Infinity];
         this.yRange = [Infinity, -Infinity];
@@ -26,53 +27,26 @@ class Game {
           this.zRange[0] = Math.min(this.zRange[0], z);
           this.zRange[1] = Math.max(this.zRange[1], z);
         });
-      }
-      make(set, [xR1, xR2, yR1, yR2, zR1, zR2] = new Array(6).fill(undefined)) {
-        const self = this;
-        let [mX, mY, mZ, mxX, mxY, mxZ] = [...set[0], ...set[0]];
-        if ([xR1, xR2, yR1, yR2, zR1, zR2].every((e) => e !== undefined)) {
-          [mX, mxX, mY, mxY, mxZ, mxZ] = [xR1, xR2, yR1, yR2, zR1, zR2];
-        } else {
-          set.forEach(([x, y, z]) => {
-            (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
-            (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
-            (mZ = Math.min(mZ, z)), (mxZ = Math.max(mxZ, z));
-          });
-        }
-        const dX = (mxX - mX) / 2 + mX;
-        const dY = (mxY - mY) / 2 + mY;
-        const dZ = (mxZ - mZ) / 2 + mZ;
-        class Node {
+        this.Node = class {
           constructor() {
-            const inSelf = this;
             this.SUB = null;
             this.xRange = [Infinity, -Infinity];
             this.yRange = [Infinity, -Infinity];
             this.zRange = [Infinity, -Infinity];
-            this.CLOUD = new Proxy([], {
-              get(target, property) {
-                if (property === "push") {
-                  return function (...args) {
-                    if (args.every((e) => e.length === 3)) {
-                      let [mX, mxX] = inSelf.xRange;
-                      let [mY, mxY] = inSelf.yRange;
-                      let [mZ, mxZ] = inSelf.zRange;
-                      args.forEach(([x, y, z]) => {
-                        (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
-                        (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
-                        (mZ = Math.min(mZ, z)), (mxZ = Math.max(mxZ, z));
-                      });
-                      inSelf.xRange = [mX, mxX];
-                      inSelf.yRange = [mY, mxY];
-                      inSelf.zRange = [mZ, mxZ];
-                    }
-                    return Array.prototype.push.apply(target, args);
-                  };
-                } else return Reflect.get(target, property);
-              },
-            });
+            this.CLOUD = [];
           }
           divide() {
+            let [mX, mxX] = this.xRange;
+            let [mY, mxY] = this.yRange;
+            let [mZ, mxZ] = this.zRange;
+            this.CLOUD.forEach(([x, y, z]) => {
+              (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
+              (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
+              (mZ = Math.min(mZ, z)), (mxZ = Math.max(mxZ, z));
+            });
+            this.xRange = [mX, mxX];
+            this.yRange = [mY, mxY];
+            this.zRange = [mZ, mxZ];
             if (this.CLOUD.length < self.LeafThreshold) return false;
             this.SUB = self.make(this.CLOUD, [
               ...this.xRange,
@@ -80,70 +54,111 @@ class Game {
               ...this.zRange,
             ]);
           }
+        };
+        this.TREE = this.make(this.DATA);
+      }
+      make(set, [xR1, xR2, yR1, yR2, zR1, zR2] = new Array(6).fill(undefined)) {
+        let [mX, mY, mZ] = set[0];
+        let [mxX, mxY, mxZ] = set[0];
+        if ([xR1, xR2, yR1, yR2, zR1, zR2].every((e) => e !== undefined)) {
+          [mX, mxX, mY, mxY, mxZ, mxZ] = [xR1, xR2, yR1, yR2, zR1, zR2];
+        } else {
+          for (const [x, y, z] of set) {
+            (mX = Math.min(mX, x)), (mxX = Math.max(mxX, x));
+            (mY = Math.min(mY, y)), (mxY = Math.max(mxY, y));
+            (mZ = Math.min(mZ, z)), (mxZ = Math.max(mxZ, z));
+          }
         }
-        let Nodes = new Array(8).fill(null).map(() => new Node());
+        const dX = (mxX + mX) / 2;
+        const dY = (mxY + mY) / 2;
+        const dZ = (mxZ + mZ) / 2;
+        const Nodes = new Array(8).fill(null).map(() => new this.Node());
         set.forEach(([x, y, z]) => {
           const index = (x > dX ? 1 : 0) | (y > dY ? 2 : 0) | (z > dZ ? 4 : 0);
           Nodes[index].CLOUD.push([x, y, z]);
         });
-        Nodes = Nodes.filter((Node) => {
+        return Nodes.filter((Node) => {
           if (Node.CLOUD.length > 0) {
             Node.divide();
             return true;
           } else return false;
         });
-        return Nodes;
       }
       search(point, set = this.TREE) {
         let [x, y, z] = point;
-        x = Math.max(this.xRange[0], Math.min(this.xRange[1], x));
-        y = Math.max(this.yRange[0], Math.min(this.yRange[1], y));
-        z = Math.max(this.zRange[0], Math.min(this.zRange[1], z));
+        const clamp = (v, min, max) => ((v > min ? v : min) < max ? v : max);
+        x = clamp(x, this.xRange[0], this.xRange[1]);
+        y = clamp(y, this.yRange[0], this.yRange[1]);
+        z = clamp(z, this.zRange[0], this.zRange[1]);
+        let includes = set[0];
         for (let i = 0; i < set.length; i++) {
-          const { xRange, yRange, zRange } = set[i];
+          const xR = set[i].xRange;
+          const yR = set[i].yRange;
+          const zR = set[i].zRange;
           if (
-            x >= xRange[0] &&
-            x <= xRange[1] &&
-            y >= yRange[0] &&
-            y <= yRange[1] &&
-            z >= zRange[0] &&
-            z <= zRange[1]
+            x >= xR[0] &&
+            x <= xR[1] &&
+            y >= yR[0] &&
+            y <= yR[1] &&
+            z >= zR[0] &&
+            z <= zR[1]
           ) {
-            if (!set[i].SUB) return this.closest([x, y, z], set[i].CLOUD);
-            else if (set[i].SUB) {
-              this.PREVIOUS = set[i];
-              return this.search([x, y, z], set[i].SUB);
-            }
-          } else if (this.PREVIOUS)
-            return this.closest([x, y, z], this.PREVIOUS.CLOUD);
+            includes = set[i];
+            break;
+          }
         }
+        if (!includes) includes = this.PREVIOUS || set[0];
+
+        // for (let i = 0; i < set.length; i++) {
+        //   const xRange = set[i].xRange;
+        //   const yRange = set[i].yRange;
+        //   const zRange = set[i].zRange;
+        //   if (
+        //     x >= xRange[0] &&
+        //     x <= xRange[1] &&
+        //     y >= yRange[0] &&
+        //     y <= yRange[1] &&
+        //     z >= zRange[0] &&
+        //     z <= zRange[1]
+        //   ) {
+        //     if (!set[i].SUB) return this.closest([x, y, z], set[i].CLOUD);
+        //     else if (set[i].SUB) {
+        //       this.PREVIOUS = set[i];
+        //       return this.search([x, y, z], set[i].SUB);
+        //     }
+        //   } else if (this.PREVIOUS)
+        //     return this.closest([x, y, z], this.PREVIOUS.CLOUD);
+        // }
+
         const { xRange, yRange, zRange } = set[0];
-        x = Math.max(xRange[0], Math.min(xRange[1], x));
-        y = Math.max(yRange[0], Math.min(yRange[1], y));
-        z = Math.max(zRange[0], Math.min(zRange[1], z));
+        x = clamp(x, xRange[0], xRange[1]);
+        y = clamp(y, yRange[0], yRange[1]);
+        z = clamp(z, zRange[0], zRange[1]);
         if (!set[0].SUB) return this.closest([x, y, z], set[0].CLOUD);
         else return this.search([x, y, z], set[0].SUB);
       }
-      closest(point, set) {
+      closest([iX, iY, iZ], set) {
         this.PREVIOUS = undefined;
         let match = set[0];
-        let matchDist = null;
-        const [iX, iY, iZ] = point;
-        if (set.some(([x, y, z]) => x === iX && y === iY && z === iZ))
-          return point;
-        set.forEach(([x, y, z]) => {
-          const distance = Math.sqrt(
-            Math.pow(x - iX, 2) + Math.pow(y - iY, 2) + Math.pow(z - iZ, 2)
-          );
-          if (!matchDist) matchDist = distance;
-          else if (distance < matchDist) {
-            matchDist = distance;
-            match = [x, y, z];
+        let matchDist = Infinity;
+        const compare = outSelf.RGBto24bit(iX, iY, iZ);
+        for (let i = 0; i < set.length; i++) {
+          const x = set[i][0],
+            y = set[i][1],
+            z = set[i][2];
+          if (outSelf.RGBto24bit(x, y, z) === compare) return [iX, iY, iZ];
+          else {
+            const distance = (x - iX) ** 2 + (y - iY) ** 2 + (z - iZ) ** 2;
+            if (distance < matchDist) {
+              matchDist = distance;
+              match = set[i];
+            }
           }
-        });
+        }
         return match;
       }
     };
+
     this.Quadtree = class {
       constructor(dataset) {
         this.DATA = dataset;
@@ -275,9 +290,7 @@ class Game {
       render() {
         const { x, y } = this;
       }
-      move() {
-        
-      }
+      move() {}
       get x() {
         return this._x;
       }
